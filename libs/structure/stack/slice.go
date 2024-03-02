681de55c5cb8bfabacce2063121ca423ce48570b
Alexander Deider <arth.inbox@gmail.com>
2024-03-02 13:19:23 +0700

    Sync concurrency

--- libs/structure/stack/slice.go
+++ libs/structure/stack/slice.go
@@ -2,26 +2,37 @@ package stack
 
 import (
 	"errors"
+	"sync"
 )
 
 var _ Stack[any] = (*Slice[any])(nil)
 
 type Slice[T any] struct {
 	elements []T
+
+	lock sync.Mutex
 }
 
 func NewSlice[T any]() *Slice[T] {
-	stack := &Slice[T]{}
-	stack.elements = make([]T, 0)
+	stack := &Slice[T]{
+		elements: make([]T, 0),
+		lock:     sync.Mutex{},
+	}
 
 	return stack
 }
 
 func (s *Slice[T]) Push(el T) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
 	s.elements = append(s.elements, el)
 }
 
 func (s *Slice[T]) Pop() (el T, err error) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
 	if len(s.elements) == 0 {
 		return el, errors.New("empty stack")
 	}
--- libs/structure/stack/slice_test.go
+++ libs/structure/stack/slice_test.go
@@ -3,11 +3,14 @@ package stack_test
 import (
 	"github.com/stretchr/testify/assert"
 	"github.com/vpArth/go-sicp/libs/structure/stack"
+	"sync"
 	"testing"
 )
 
 func TestSlice(t *testing.T) {
+	t.Parallel()
 	t.Run("PushPop", func(t *testing.T) {
+		t.Parallel()
 		s := stack.NewSlice[int]()
 		s.Push(1)
 		s.Push(2)
@@ -23,5 +26,43 @@ func TestSlice(t *testing.T) {
 		assert.Equal(t, 1, el)
 		_, err := s.Pop()
 		assert.EqualError(t, err, "empty stack")
+	})
+
+	t.Run("Concurrency", func(t *testing.T) {
+		t.Parallel()
+
+		s := stack.NewSlice[int]()
+		const N = 100
+
+		var wgPush sync.WaitGroup
+		wgPush.Add(N)
+		for i := 0; i < N; i++ {
+			go func(num int) {
+				defer wgPush.Done()
+				s.Push(num)
+			}(i + 1)
+		}
+		wgPush.Wait()
+
+		sum := 0
+		var (
+			wgPop sync.WaitGroup
+			mux   sync.Mutex
+		)
+
+		wgPop.Add(N)
+		for i := 0; i < N; i++ {
+			go func() {
+				defer wgPop.Done()
+				el, _ := s.Pop()
+
+				mux.Lock()
+				sum += el
+				mux.Unlock()
+			}()
+		}
+		wgPop.Wait()
+
+		assert.Equal(t, 5050, sum)
 	})
 }
